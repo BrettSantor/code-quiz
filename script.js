7b3b4551c772d790d1bc1caddb678adb4a11b2a8
BrettSantor <b.santor27@gmail.com>
2022-12-26 13:22:25 -0800

    add comments changed timer start

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//create objects with an array of keys to be called by index
 var questions = [
     {text:"question1", choices:["a", "b", "c", "d"], correct: "a"},
     {text:"question2", choices:["a", "b", "c", "d"], correct: "a"},
@@ -6,25 +7,28 @@ var questions = [
     {text:"question5", choices:["a", "b", "c", "d"], correct: "a"},
     {text:"question6", choices:["a", "b", "c", "d"], correct: "a"},
 ];
+//variable to iterate through questions
 var Q = 0;
+//variable to target span tag for timer
 var timeEl = document.querySelector(".timer");
+//variable used to decrease time
 var secondsLeft = 60;
 
-//create timer
 function startGame(){
-    //start timer
-   // hides start screen
-    document.querySelector(".start").classList.add("hide")
-    //show quiz screen
+   // hides start screen >
+   document.querySelector(".start").classList.add("hide")
+   //show quiz screen >
     document.querySelector(".quiz").classList.remove("hide")
     //call function to load first question
 
-}
+}//create timer >
 function setTimer() {
+    //setInterval method repeatedly executes code with a fixed delay each execution
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
         if(secondsLeft === 0) {
+    //clearInterval method stops the called action set in place by setInterval method
             clearInterval(timerInterval);
         }
     }, 1000);
@@ -41,4 +45,5 @@ function setTimer() {
 //save score and initials to local storage
 //when user hits high scores show list of high scores
 document.querySelector(".start-btn").addEventListener("click", startGame)
+//start timer
 document.querySelector(".start-btn").addEventListener("click", setTimer)
